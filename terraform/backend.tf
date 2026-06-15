1c9bf2fd7e43bed31e99545a1fb8f56df2581029
John Jonah <johnjonah0866@gmail.com>
2026-06-15 21:35:33 +0000

    modernize terraform s3 backend locking

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-vmware-migration-0fce790f"
-    key            = "cloud-platform-migration-lab/terraform.tfstate"
-    region         = "eu-north-1"
-    dynamodb_table = "terraform-state-locks"
-    encrypt        = true
+    bucket       = "terraform-state-vmware-migration-0fce790f"
+    key          = "cloud-platform-migration-lab/terraform.tfstate"
+    region       = "eu-north-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
